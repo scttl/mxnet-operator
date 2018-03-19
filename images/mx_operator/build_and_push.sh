eebedbde35d44cb621a264c902d83168f17beb18
Scott Leishman <scott.leishman@gmail.com>
2018-03-19 16:58:11 -0700

    bugfixes and cluster IP updates required for deployment.

--- images/mx_operator/build_and_push.sh
+++ images/mx_operator/build_and_push.sh
@@ -9,6 +9,7 @@ ROOT_DIR=${SRC_DIR}/../../
 # The image tag is based on the githash.
 GITHASH=$(git rev-parse --short HEAD)
 CHANGES=$(git diff-index --quiet HEAD -- || echo "untracked")
+CHANGES=""
 if [ -n "$CHANGES" ]; then
   # Get the hash of the diff.
   DIFFHASH=$(git diff  | sha256sum)
@@ -18,13 +19,13 @@ fi
 
 DIR=`mktemp -d`
 echo Use ${DIR} as context
-go install github.com/deepinsight/mxnet-operator/cmd/mx_operator
-go install github.com/deepinsight/mxnet-operator/test/e2e
+#go install github.com/deepinsight/mxnet-operator/cmd/mx_operator
+#go install github.com/deepinsight/mxnet-operator/test/e2e
 cp ${GOPATH}/bin/mx_operator ${DIR}/
 cp ${GOPATH}/bin/e2e ${DIR}/
 cp ${SRC_DIR}/Dockerfile ${DIR}/
-REGISTRY=10.199.192.16
-IMAGE=${REGISTRY}/tensorflow/mx_operator:1.7-${GITHASH}
-docker build --build-arg http_proxy=http://172.16.22.7:8118 -t $IMAGE -f ${DIR}/Dockerfile ${DIR}
-docker push $IMAGE
+REGISTRY=gcr.io/constant-cubist-173123
+IMAGE=${REGISTRY}/mx_operator:1.7-${GITHASH}
+docker build -t $IMAGE -f ${DIR}/Dockerfile ${DIR}
+gcloud docker -- push $IMAGE
 echo pushed $IMAGE
